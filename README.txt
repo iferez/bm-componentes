1ea58ef6aafff6f91d297f6e506f2ec7b757b126
Ivan Ferez <ferezivan@hotmail.com>
2024-05-24 14:57:29 -0300

    Nuevos componentes mayo

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Cambios en:
-CSS accordion - banner - cards  - global (tipografia - nuevo boton header) - header y footer fix - slick (slider padding)
+CSS accordion - banner - cards  - global (tipografia - nuevo boton header - container stop) - header y footer fix - slick (slider padding)
+CSS carrusel - product 
 JS scroll - sliders
--- js/sliders.js
+++ js/sliders.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const originalChildCounts = {};
+
   function verifyAndActivateSliders() {
     /* Verifico si se debe activar - envio el ID del html y cuantos elementos tiene que haber para armar el slider */
     var promoSliderOn = verifySlider("slider-promo", 5);
     var pasosSliderOn = verifySlider("slider-pasos", 5);
     var productosSliderOn = verifySlider("slider-productos", 5);
     var gestionSliderOn = verifySlider("slider-gestion", 5);
-    var gestionSliderOn = verifySlider("slider-nuevo", 5);
-    
+    var beneficiosSliderOn = verifySlider("slider-beneficios", 5);
+    var carusselSliderOn = verifySlider("slider-carrusel-info", 4);
 
     /* Armo o desarmo el slider - envio ID + validacion  */
     toggleSlider("slider-promo", promoSliderOn);
     toggleSlider("slider-pasos", pasosSliderOn);
     toggleSlider("slider-productos", productosSliderOn);
     toggleSlider("slider-gestion", gestionSliderOn);
-    toggleSlider("slider-nuevo", gestionSliderOn);
+    toggleSlider("slider-beneficios", beneficiosSliderOn);
+    toggleSlider("slider-carrusel-info", carusselSliderOn);
   }
 
   function verifySlider(sliderId, slidesToShow) {
@@ -22,12 +25,15 @@ document.addEventListener("DOMContentLoaded", function () {
       return false;
     }
     
-    var children = Array.from(sliderWrapper.childNodes).filter(node => node.nodeType === 1);
-    return (
-        children.length >= slidesToShow || window.innerWidth < 992
-    );
-}
+    if (!originalChildCounts[sliderId]) {
+      var children = Array.from(sliderWrapper.childNodes).filter(node => node.nodeType === 1);
+      originalChildCounts[sliderId] = children.length;
+    }
 
+    return (
+      originalChildCounts[sliderId] >= slidesToShow || window.innerWidth < 992
+    );
+  }
 
   function toggleSlider(sliderId, shouldActivate) {
     var sliderWrapper = document.getElementById(sliderId);
@@ -39,8 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
       sliderWrapper.classList.add("convert-slider");
       initializeSlider(sliderId);
     } else {
-      var isSliderInitialized =
-        sliderWrapper.classList.contains("slick-initialized");
+      var isSliderInitialized = sliderWrapper.classList.contains("slick-initialized");
       if (isSliderInitialized) {
         $("#" + sliderId).slick("unslick");
         sliderWrapper.classList.remove("convert-slider");
@@ -50,23 +55,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function initializeSlider(sliderId) {
     var sliderWrapper = document.getElementById(sliderId);
-    var isSliderInitialized =
-      sliderWrapper.classList.contains("slick-initialized");
+    var isSliderInitialized = sliderWrapper.classList.contains("slick-initialized");
 
     if (!isSliderInitialized) {
       $("#" + sliderId).slick({
-        slidesToShow: 4,
+        slidesToShow: sliderId == "slider-carrusel-info" ? 3 : 4,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
+        autoplay: sliderId  == "slider-carrusel-info" ? false : true,
+        autoplaySpeed: sliderId == "slider-promo" ? 2000 : 3000,
         focusOnSelect: true,
         variableWidth: true,
-        arrows: true,
+        arrows: false,
         dots: true,
         responsive: [
           {
             breakpoint: 996,
             settings: {
+              autoplay: true,
               slidesToShow: 2,
               slidesToScroll: 1,
               arrows: false,
